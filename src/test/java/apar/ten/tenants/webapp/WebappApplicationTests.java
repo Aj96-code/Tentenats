401504f84a713d02ed9833f1f0c1b172ead8cf02
Aj96-code <thompsonAziel0@gmail.com>
2023-08-12 14:40:50 -0500

    Initial Commit | Basic Project Setup

---
+++ src/test/java/apar/ten/tenants/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package apar.ten.tenants.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
