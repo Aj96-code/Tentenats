9e39b1fd368566155ff370ca1bcb58bb3495dabf
Aj96-code <thompsonAziel0@gmail.com>
2023-08-12 19:28:13 -0500

    Adjusted Banner and Webpage Title

--- src/main/java/apar/ten/tenants/webapp/controller/HomeController.java
+++ src/main/java/apar/ten/tenants/webapp/controller/HomeController.java
@@ -1,20 +1,14 @@
 package apar.ten.tenants.webapp.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  @Value("${info.app.name}")
-  private String title;
-  @Value("${info.app.description}")
-  private String description;
-
  @GetMapping("/")
  public String home(Model thModel){
-  thModel.addAttribute("title",title + " | "+description);
+  thModel.addAttribute("title", "Home Page");
   return "index";
  } 
 }
