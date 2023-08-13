348c2a810174ef32eadca70f51a823912450b12e
Aj96-code <thompsonAziel0@gmail.com>
2023-08-12 19:22:13 -0500

    Added Changeable Layouts

---
+++ src/main/java/apar/ten/thymeleaf/ThymeleafConfiguration.java
@@ -0,0 +1,16 @@
+package apar.ten.thymeleaf;
+
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
+@Configuration
+public class ThymeleafConfiguration {
+
+  public SpringTemplateEngine thymeleafLayoutDialect(){
+    SpringTemplateEngine engine = new SpringTemplateEngine();
+    engine.addDialect( new LayoutDialect());
+    return engine;
+  }
+}
